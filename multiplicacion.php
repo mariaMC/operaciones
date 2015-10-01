47e599a62b9cfa2b2ff82cb7d6c37029d5af01c2
root <root@mary.localhost>
2015-10-01 15:02:05 -0500

    cosntructor

--- multiplicacion.php
+++ multiplicacion.php
@@ -1,30 +1,51 @@
 <?php
 class Operaciones {
+
 	public $num1;
 	public $num2;
   public $resultado;
-	
-  public function numeros($a,$b){
+
+  function __construct($a,$b){
     $this->num1 = $a;
     $this->num2 = $b;
-    	
+  }
+	
+   public function sumar(){
+    $this->resultado = $this->num1 + $this->num2;
+    return $this->resultado;
+
   }
 
-  private function multiplica(){
+   public function restar(){
+    $this->resultado = $this->num1 - $this->num2;
+
+  }
+
+  public function multiplicar(){
   	$this->resultado = $this->num1 * $this->num2;
 
   }
 
   public function imprimirResultado(){
-    $this->multiplica();
     return $this->resultado;
   }
   
 }
 
-$multiplicacion = new Operaciones;
-$multiplicacion->numeros(2,5);
-echo "El resultado es: ".$multiplicacion->imprimirResultado();
+$suma = new Operaciones(3,3);
+$suma->sumar();
+echo "La suma es: ".$suma->imprimirResultado();
+echo "<br>";
+
+$resta = new Operaciones(10,3);
+$resta->restar();
+echo "La resta es: ".$resta->imprimirResultado();
+echo "<br>";
+
+$multiplicacion = new Operaciones(5,3);
+$multiplicacion->multiplicar();
+echo "\nLa multiplicacion es: ".$multiplicacion->imprimirResultado();
+
 
 
 ?>
